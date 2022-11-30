c0145564425d3a376952762fc26ffbd512c18867
hkursatsimsek <kursat.simsek@modanisa.com>
2022-12-01 00:24:30 +0300

    app started with storyboard design

---
+++ GoodWeather-MVVM/Controllers/AddWeatherCityViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddWeatherCityViewController.swift
+//  GoodWeather-MVVM
+//
+//  Created by Kürşat Şimşek on 30.11.2022.
+//
+
+import Foundation
+import UIKit
+
+class AddWeatherCityViewController: UIViewController {
+    
+}
---
+++ GoodWeather-MVVM/Controllers/WeatherListTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherListTableViewController.swift
+//  GoodWeather-MVVM
+//
+//  Created by Kürşat Şimşek on 30.11.2022.
+//
+
+import Foundation
+import UIKit
+
+class WeatherListTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+    }
+    
+}
